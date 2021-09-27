251f86a426f609054f2790a9031da04eba0e96bc
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-09-27 17:47:32 +0300

    First commit

---
+++ src/screens/Main.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+export default function Main() {
+    const [num, setNum] = useState(undefined);
+    const [result, setResult] = useState();
+    const [requestedNum, setRequestedNum] = useState();
+
+    const inputValidation = num === undefined || isNaN(num);
+
+    const postNum = async () => {
+        const result = await fetch("http://localhost:5000/api/numbers", {
+            method: "POST",
+            body: JSON.stringify({
+                num
+            }),
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+        return result.json();
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const { result: answer, num: yourNum } = await postNum();
+        setResult(answer);
+        setRequestedNum(yourNum);
+        setNum('');
+    };
+
+    return (
+        <div className="main-container">
+            <form onSubmit={(e) => submitHandler(e)}>
+                <label htmlFor="numberField">Enter your number</label>
+                <input id="numberField" type="number" value={num} onChange={({target}) => setNum(target.value)} />
+                {result &&
+                    <>
+                        <p>Your number: {requestedNum}</p>
+                        <p>Answer: {result}</p>
+                    </>
+                }
+                <button disabled={inputValidation} className={inputValidation ? 'disabled' : 'run-button'} type="submit">Run</button>
+            </form>
+        </div>
+    )
+}
